41071b48156a5132e89bf4a2765293a63a2fc4d8
109mastert <master15820@gmail.com>
2022-10-20 00:33:16 +0200

    Navbar patch 2.0

--- javascript/Homepage.js
+++ javascript/Homepage.js
@@ -5,17 +5,18 @@ const homeButton = document.querySelector(".homeButton");
 
 loadText();
 
-function loadText(){
-    mainTitle.style.transform = "translateY(0)";
-    mainTitle.style.opacity = "1";
-    subtitle.style.transform = "translateY(0)";
-    subtitle.style.opacity = "1";
-    menuButton.style.opacity = "1";
-    // menuButton.style.transform = "translate(0,0)";
-    homeButton.style.top = "0";
-    homeButton.style.left = "0";
-    homeButton.style.transform = "scale(1)";
-    homeButton.style.opacity = "1";
-    setTimeout( () => homeButton.style.position = "static",2000);
-
+function loadText() {
+  menuButton.style.opacity = "0";
+  mainTitle.style.transform = "translateY(0)";
+  mainTitle.style.opacity = "1";
+  subtitle.style.transform = "translateY(0)";
+  subtitle.style.opacity = "1";
+  menuButton.style.opacity = "1";
+  // menuButton.style.transform = "translate(0,0)";
+  homeButton.style.top = "0";
+  homeButton.style.left = "0";
+  homeButton.style.transform = "scale(1)";
+  homeButton.style.opacity = "1";
+  setTimeout(() => (homeButton.style.position = "static"), 2000);
+  setTimeout(() => (menuButton.style.opacity = "1"), 2000);
 }
